8cb9c3d28cf8184691a24f60910e139d8ca94e4c
Rarestdiligence15 <lorriejason4@gmail.com>
2023-06-15 09:46:05 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-/**
-*main - Entry point of program
-*
-*return: 0 on success, non-zero value on failure
-*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
-
